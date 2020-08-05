cfc30bb641684e18cf7cdfc910899313e3a9cc58
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-08-05 20:07:49 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,6 +6,8 @@ class GetRequester
   URL = https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json
   
   def get_response_body
+    uri = URI.parse(URL)
+    
   end
   
   def parse_json
